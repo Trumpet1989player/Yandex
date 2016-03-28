0e98e410c2974124d1503e8b39b99faf02203115
NaumenkoP <thepauloo89@gmail.com>
2016-03-28 11:52:43 +0300

    littleFix

--- src/test/java/Yandex.java
+++ src/test/java/Yandex.java
@@ -33,17 +33,20 @@ public class  Yandex {
 //        TranslatePage translatePage = mainPage.mainMenu.openTanslatePage();
 //        translatePage.search("Hello");
         //mainPage.widgets.clickToWeather();
-        WebElement penis =
+        WebElement pogodaWi =
                 driver.findElement(By.xpath("//*[@id=\"wd-_weather\"]/div/h1/a[2]"));
-        new Actions(driver).moveToElement(penis).perform();
+        new Actions(driver).moveToElement(pogodaWi).perform();
         WebDriverWait wait = new WebDriverWait(driver, 3);
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"wd-wrapper-_weather\"]/div[2]/a/div"))).click();
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"widget_pref_form\"]/div/div[3]/div/div[1]/button[2]"))).click();
-        WebElement movePenis =
+        WebElement movePogodaWi =
                 driver.findElement(By.xpath("//*[@id=\"wd-wrapper-_weather-1\"]/div[3]"));
-        WebElement staticpenis = driver.findElement(By.xpath("//*[@id=\"wd-wrapper-_geo-1\"]/div[3]"));
-        new Actions(driver).dragAndDrop(movePenis,staticpenis);
-                //dragAndDropBy(movePenis,50,0);
+        WebElement staticWi = driver.findElement(By.xpath("//*[@id=\"wd-wrapper-_geo-1\"]/div[3]"));
+        Actions hold = new Actions(driver);
+        hold.perform();
+        hold.clickAndHold().moveByOffset(50,0);
+                //dragAndDrop(movePogodaWi,staticWi);
+                //dragAndDropBy(movePogodaWi,50,0);
         //driver.findElement(By.xpath("//*[@id=\"widget_pref_form\"]/div/div[3]/div/div[1]/button[2]/span")).click();
 //     driver.findElement(By.xpath()).click();
     }
